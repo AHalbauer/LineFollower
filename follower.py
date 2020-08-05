4c610179cd43c24182bd563c279adc97eac271b4
AHalbauer <33034577+AHalbauer@users.noreply.github.com>
2020-08-05 14:42:30 +0200

    Update follower.py

--- follower.py
+++ follower.py
@@ -9,7 +9,6 @@ class FOLLOWER():
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         
-        # init modules
         self.motor = L293D(motor_left_pin1, motor_left_pin2, motor_right_pin1, motor_right_pin2)
         self.line_follow_pin_left  = line_follow_pin_left
         self.line_follow_pin_right = line_follow_pin_right
@@ -17,7 +16,7 @@ class FOLLOWER():
         GPIO.setup(self.line_follow_pin_left,  GPIO.IN)
         GPIO.setup(self.line_follow_pin_right, GPIO.IN)
     
-    def lineFollowModeOn(self):
+    def followLine(self):
      
         status_left  = False
         status_right = False
@@ -28,33 +27,36 @@ class FOLLOWER():
             
             if status_left and status_right:
                 self.motor.forward()
+                print("Just follow your nose")
             elif status_left:
                 self.motor.forwardR()
+                print("move to the right")
             elif status_right:
                 self.motor.forwardL()
+                print("move to the left")
             else:
                 self.motor.backward()
                 time.sleep(7.5/self.motor.CM_PER_SEC)
                 self.motor.stop()
-                degrees_to_search = 45.0
+                search_degree = 45.0
                 self.motor.forwardR()
-                s = GPIO.wait_for_edge(self.line_follow_pin_left, GPIO.RISING, timeout=int(1000 * self.motor.SEC_PER_TURN / 360.0 * degrees_to_search))
+                s = GPIO.wait_for_edge(self.line_follow_pin_left, GPIO.RISING, timeout=int(1000 * self.motor.SEC_PER_TURN / 360.0 * search_degree))
                 self.motor.stop()
                 if s is not None:
                     continue
                 else:
                     self.motor.backwardR()
-                    time.sleep(self.motor.SEC_PER_TURN / 360.0 * degrees_to_search)
+                    time.sleep(self.motor.SEC_PER_TURN / 360.0 * search_degree)
                     self.motor.forwardL()
-                    s = GPIO.wait_for_edge(self.line_follow_pin_right, GPIO.RISING, timeout=int(1000 * self.motor.SEC_PER_TURN / 360.0 * degrees_to_search))
+                    s = GPIO.wait_for_edge(self.line_follow_pin_right, GPIO.RISING, timeout=int(1000 * self.motor.SEC_PER_TURN / 360.0 * search_degree))
                     self.motor.stop()
                     if s is not None:
-                        print("fund")
+                        print("back on track")
                         continue
                     else:
                         self.motor.backwardL()
-                        time.sleep(self.motor.CM_PER_TURN / 360.0 * degrees_to_search)
+                        time.sleep(self.motor.CM_PER_TURN / 360.0 * search_degree)
                         self.motor.stop()
                         break
-            time.sleep(0.001)
+            time.sleep(0.1)
     
